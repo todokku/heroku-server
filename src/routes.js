2ad7ac5498b42e24b0d3a001566a0158d0a20bf4
gabrielkf <gabrielkf@gmail.com>
2020-05-02 02:49:55 -0300

    added async await on routes

--- src/routes.js
+++ src/routes.js
@@ -9,22 +9,24 @@ routes.get('/', (req, res) => {
 });
 
 /* DATABASE */
-routes.get('/products', (req, res) => {
-  knex
+routes.get('/products', async (req, res) => {
+  await knex
     .raw('SELECT * from products')
     .then(function (products) {
       res.send(products.rows);
     });
 });
 
-routes.get('/stock', (req, res) => {
-  knex.raw('SELECT * from stock').then(function (stock) {
-    res.send(stock.rows);
-  });
+routes.get('/stock', async (req, res) => {
+  await knex
+    .raw('SELECT * from stock')
+    .then(function (stock) {
+      res.send(stock.rows);
+    });
 });
 
-routes.get('/sellers', (req, res) => {
-  knex
+routes.get('/sellers', async (req, res) => {
+  await knex
     .raw('SELECT * from sellers')
     .then(function (sellers) {
       res.send(sellers.rows);
