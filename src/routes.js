c49f3db53618c140be81fb280a330bcb30e5ccd7
gabrielkf <gabrielkf@gmail.com>
2020-05-02 02:15:01 -0300

    Fixed routes.

--- src/routes.js
+++ src/routes.js
@@ -3,16 +3,32 @@ const knex = require('../db/knex');
 
 const routes = Router();
 
-/* GET home page. */
+/* HOME PAGE */
 routes.get('/', (req, res) => {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Team13' });
 });
 
-/* GET home page. */
-routes.get('/api', (req, res) => {
-  knex.raw('SELECT * from todos').then(function (todos) {
-    res.send(todos.rows);
+/* DATABASE */
+routes.get('/products', (req, res) => {
+  knex
+    .raw('SELECT * from products')
+    .then(function (products) {
+      res.send(products.rows);
+    });
+});
+
+routes.get('/stock', (req, res) => {
+  knex.raw('SELECT * from stock').then(function (stock) {
+    res.send(stock.rows);
   });
+});
+
+routes.get('/sellers', (req, res) => {
+  knex
+    .raw('SELECT * from sellers')
+    .then(function (sellers) {
+      res.send(sellers.rows);
+    });
 });
 
 module.exports = routes;
